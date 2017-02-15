e432d1e5106e121e27b512f490d1add0f5967f5b
Alexander Moskovskiy <avmoskovskiy@365w418294.local>
2017-02-15 12:37:21 +0300

    можно кликать

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,7 +5,7 @@ $(function() {
 		// document.querySelector(element).style.width = Math.floor(Math.random()*(100-20)+20)+'%';
 		// document.querySelector(element).style.height = Math.floor(Math.random()*(100-20)+20)+'%';
 		// document.querySelector(element).style.backgroundColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-		document.querySelector(element).style.transform = "rotate("+Math.floor(Math.random()*360)+'deg'+')'+' '+'translateX('+Math.floor(Math.random()*(100-0)+0)+'%'+')'+' '+'translateY('+Math.floor(Math.random()*(100-0)+0)+'%'+')';
+		document.querySelector(element).style.transform = 'rotate(' + Math.floor(Math.random()*360) + 'deg) translateX(' + Math.floor(Math.random()*(100-0)+0) + '%) translateY(' + Math.floor(Math.random()*(100-0)+0) + '%)';
 	}
 
 	// rectangle('.rectangle.f1');
@@ -16,6 +16,16 @@ $(function() {
 	// rectangle('.rectangle.f6');
 	// rectangle('.rectangle.f7');
 	// document.querySelector('.screen').style.backgroundColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+
+	$('.elements').click(function() {
+		rectangle('.rectangle.f1');
+		rectangle('.rectangle.f2');
+		rectangle('.rectangle.f3');
+		rectangle('.rectangle.f4');
+		rectangle('.rectangle.f5');
+		rectangle('.rectangle.f6');
+		rectangle('.rectangle.f7');
+	});
 
 	var button = document.querySelector('.newWallpaper');
 	document.onkeyup = (e) => {
@@ -28,15 +38,6 @@ $(function() {
 			rectangle('.rectangle.f6');
 			rectangle('.rectangle.f7');
 
-		$('.elements').click(function() {
-			rectangle('.rectangle.f1');
-			rectangle('.rectangle.f2');
-			rectangle('.rectangle.f3');
-			rectangle('.rectangle.f4');
-			rectangle('.rectangle.f5');
-			rectangle('.rectangle.f6');
-			rectangle('.rectangle.f7');
-	  });
 		}
 	}
 });
