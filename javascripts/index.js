0de5ad390af81c2ae44c0ad924b247410ca3edc5
Alexander Moskovskiy <aleksandrmoskovskiy@iMac-Aleksandr.local>
2017-02-25 20:25:56 +0300

    изменил алгоритм рандома, работает кнопка возврата к оригиналу

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,7 +1,7 @@
 $(function() {
 	var rectangle = (element) => {
-		document.querySelector(element).style.left = Math.floor(Math.random()*(40-20)+20)+'%';
-		document.querySelector(element).style.top = Math.floor(Math.random()*(40-20)+20)+'%';
+		document.querySelector(element).style.left = Math.floor(Math.random()*(90-10)+10)+'%';
+		document.querySelector(element).style.top = Math.floor(Math.random()*(90-10)+10)+'%';
 		// document.querySelector(element).style.width = Math.floor(Math.random()*(100-20)+20)+'%';
 		// document.querySelector(element).style.height = Math.floor(Math.random()*(100-20)+20)+'%';
 		// document.querySelector(element).style.backgroundColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
@@ -21,6 +21,7 @@ $(function() {
 		rectangle('.rectangle.f5');
 		rectangle('.rectangle.f6');
 		rectangle('.rectangle.f7');
+		rectangle('.rectangle.f8');
 	});
 
 	$('.random').click(function() {
@@ -31,6 +32,7 @@ $(function() {
 		rectangle('.rectangle.f5');
 		rectangle('.rectangle.f6');
 		rectangle('.rectangle.f7');
+		rectangle('.rectangle.f8');
 	});
 
 	var button = document.querySelector('.newWallpaper');
@@ -43,7 +45,24 @@ $(function() {
 			rectangle('.rectangle.f5');
 			rectangle('.rectangle.f6');
 			rectangle('.rectangle.f7');
-
+			rectangle('.rectangle.f8');
 		}
 	}
+});
+
+$(function() {
+	var rectangle = (element) => {
+		$('.rectangle').attr('style', '')
+	}
+
+	$('.tooriginal').click(function() {
+		rectangle('.rectangle.f1');
+		rectangle('.rectangle.f2');
+		rectangle('.rectangle.f3');
+		rectangle('.rectangle.f4');
+		rectangle('.rectangle.f5');
+		rectangle('.rectangle.f6');
+		rectangle('.rectangle.f7');
+		rectangle('.rectangle.f8');
+	});
 });
